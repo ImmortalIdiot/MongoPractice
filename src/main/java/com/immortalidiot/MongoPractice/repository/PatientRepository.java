c5511621d5bee2cdfc2f4a8127b72192ac3fcd34
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-19 21:49:08 +0300

    fix: annotate the repository with the appropriate annotation

--- src/main/java/com/immortalidiot/MongoPractice/repository/PatientRepository.java
+++ src/main/java/com/immortalidiot/MongoPractice/repository/PatientRepository.java
@@ -2,9 +2,11 @@ package com.immortalidiot.MongoPractice.repository;
 
 import com.immortalidiot.MongoPractice.model.Patient;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface PatientRepository extends MongoRepository<Patient, String> {
     List<Patient> findBySeverity(String severity);
     List<Patient> findByAge(int age);
