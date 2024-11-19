890ff66df7da835634ec79aa468bac8473d18f23
ImmortalIdiot <max.krasulya@yandex.ru>
2024-11-19 22:04:57 +0300

    fix: pass the collection name

--- src/main/java/com/immortalidiot/MongoPractice/model/Patient.java
+++ src/main/java/com/immortalidiot/MongoPractice/model/Patient.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
-@Document(collation = "Medicals")
+@Document(collection = "Medicals")
 public class Patient {
 
     @Id
